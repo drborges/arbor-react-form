d7bad33232fc6362b4f4294a4cd515350fa64d71
Diego Borges <drborges.cic@gmail.com>
2018-05-11 22:46:09 -0300

    Implement validation prototype

--- src/index.js
+++ src/index.js
@@ -1,45 +1,70 @@
-import React from "react";
+import React from "react"
 
-const bindProps = (target, props) => {
+const bindProps = (node, validate, props) => {
   const mapping = {
     radio: {
-      checked: target.value === props.value,
+      checked: node.value === props.value,
       onChange: e => {
-        target.value = e.target.value;
-        props.onChange && props.onChange(e);
+        node.value = e.target.value
+        props.onChange && props.onChange(e)
       }
     },
     checkbox: {
-      checked: target.value,
+      checked: node.value,
       onChange: e => {
-        target.value = e.target.checked;
-        props.onChange && props.onChange(e);
+        node.value = e.target.checked
+        props.onChange && props.onChange(e)
       }
     },
     default: {
-      value: target.value,
+      value: node.value,
       onChange: e => {
-        target.value = e.target.value;
-        props.onChange && props.onChange(e);
+        node.value = e.target.value
+        props.onChange && props.onChange(e)
       }
     }
   }
 
-  return mapping[props.type] || mapping.default
+  const inputProps = mapping[props.type] || mapping.default
+
+  if (validate) {
+    const handler = inputProps[validate]
+    inputProps[validate] = (e) => {
+      handler && handler(e)
+      node.valid = e.target.checkValidity()
+      node.message = e.target.validationMessage
+    }
+  }
+
+  return inputProps
 }
 
-const Control = ({ children, target }) => {
-  const controls = ["input", "select", "textarea"]
+class Control extends React.PureComponent {
+  componentDidMount() {
+    const target = this.props.target
+    target.$reset = target.$reset || (() => {
+      target.$mutate(target => {
+        target.value = ""
+        target.valid = undefined
+        target.message = undefined
+      })
+    })
+  }
 
-  return React.Children.map(children, child => {
-    if (!child || !controls.includes(child.type)) {
-      return child;
-    }
+  render() {
+    const { children, target, validate } = this.props
+    const controls = ["input", "select", "textarea"]
 
-    return React.cloneElement(child, bindProps(target, child.props));
-  });
-};
+    return React.Children.map(children, child => {
+      if (!child || !controls.includes(child.type)) {
+        return child
+      }
+
+      return React.cloneElement(child, bindProps(target, validate, child.props))
+    })
+  }
+}
 
 export default {
   Control,
-};
+}
